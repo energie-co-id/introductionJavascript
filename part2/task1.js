7bc3d7cc47b91618c84c156005bf1295be4d8381
unknown <adibmaknun16@gmail.com>
2022-03-16 09:31:20 +0700

    task 3

--- part2/task1.js
+++ part2/task1.js
@@ -1,7 +1,7 @@
-let list = [{ metode: 'filter', fungsi: 'Membuat array baru dengan setiap elemen dalam array yang lulus tes' },
-    { metode: 'find', fungsi: 'Mengembalikan nilai elemen pertama dalam larik yang lulus ujian' },
-    { metode: 'includes', fungsi: 'metode menentukan apakah array menyertakan nilai tertentu di antara entrinya' },
-    { metode: 'join', fungsi: 'Menggabungkan semua elemen array menjadi string' },
+let list = [{ metode: 'Filter', fungsi: 'Membuat array baru dengan setiap elemen dalam array yang lulus tes' },
+    { metode: 'Find', fungsi: 'Mengembalikan nilai elemen pertama dalam larik yang lulus ujian' },
+    { metode: 'Includes', fungsi: 'metode menentukan apakah array menyertakan nilai tertentu di antara entrinya' },
+    { metode: 'Join', fungsi: 'Menggabungkan semua elemen array menjadi string' },
     { metode: 'map', fungsi: 'Membuat array baru dengan hasil memanggil fungsi untuk setiap elemen array' },
     { metode: 'pop', fungsi: 'Menghapus elemen terakhir dari array, dan mengembalikan elemen itu' },
     { metode: 'push', fingsi: 'Menambahkan elemen baru ke akhir array, dan mengembalikan panjang baru' },
@@ -14,10 +14,11 @@ let listmetode = ['filter', 'find', 'includes', 'join', 'map', 'pop', 'push', 'r
 function metode(x) {
     let cek = listmetode.includes(x); //proses apakah metode tersedia di list
     if (cek == false) {
-        return ("metode tidak tersedia di library")
+        const z = listmetode.join(", ")
+        return ("metode tidak tersedia di library " + "metode yang tersedia hanya: " + z)
     } else {
         const z = list.find(met => met.metode === x); //proses pencarian metode di list
         return (z)
     }
 }
-console.log(metode('splice'))
+console.log(metode('toLowerCase'))
