3f06c4a3af8b02038d16ee9dd2c367b428002245
unknown <adibmaknun16@gmail.com>
2022-03-16 09:20:33 +0700

    task 3

--- part2/task1.js
+++ part2/task1.js
@@ -1,15 +1,15 @@
-let list = [{ metode: 'filter', fungsi: 'Creates a new array with every element in an array that pass a test' },
-    { metode: 'find', fungsi: 'Returns the value of the first element in an array that pass a test' },
-    { metode: 'from', fungsi: 'Creates an array from an object' },
-    { metode: 'join', fungsi: 'Joins all elements of an array into a string' },
-    { metode: 'map', fungsi: 'Creates a new array with the result of calling a function for each array element' },
-    { metode: 'pop', fungsi: 'Removes the last element of an array, and returns that element' },
-    { metode: 'push', fingsi: 'Adds new elements to the end of an array, and returns the new length' },
-    { metode: 'reduce', fungsi: 'Reduce the values of an array to a single value' },
-    { metode: 'reverse', fungsi: 'Reverses the order of the elements in an array' },
-    { metode: 'splice', fungsi: 'Adds/Removes elements from an array' }
+let list = [{ metode: 'filter', fungsi: 'Membuat array baru dengan setiap elemen dalam array yang lulus tes' },
+    { metode: 'find', fungsi: 'Mengembalikan nilai elemen pertama dalam larik yang lulus ujian' },
+    { metode: 'includes', fungsi: 'metode menentukan apakah array menyertakan nilai tertentu di antara entrinya' },
+    { metode: 'join', fungsi: 'Menggabungkan semua elemen array menjadi string' },
+    { metode: 'map', fungsi: 'Membuat array baru dengan hasil memanggil fungsi untuk setiap elemen array' },
+    { metode: 'pop', fungsi: 'Menghapus elemen terakhir dari array, dan mengembalikan elemen itu' },
+    { metode: 'push', fingsi: 'Menambahkan elemen baru ke akhir array, dan mengembalikan panjang baru' },
+    { metode: 'reduce', fungsi: 'Kurangi nilai array menjadi nilai tunggal' },
+    { metode: 'reverse', fungsi: 'Membalikkan urutan elemen dalam array' },
+    { metode: 'splice', fungsi: 'Menambahkan/Menghapus elemen dari array' }
 ]
-let listmetode = ['filter', 'find', 'from', 'join', 'map', 'pop', 'push', 'reduce', 'reverse', 'splice']
+let listmetode = ['filter', 'find', 'includes', 'join', 'map', 'pop', 'push', 'reduce', 'reverse', 'splice']
 
 function metode(x) {
     let cek = listmetode.includes(x); //proses apakah metode tersedia di list
