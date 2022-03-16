04050f65e43adc22ffa8088d6bc59607c18b3a3c
unknown <adibmaknun16@gmail.com>
2022-03-16 11:19:20 +0700

    task 3

--- part2/task2.js
+++ part2/task2.js
@@ -9,19 +9,21 @@ const list = ['a', 'b', 'c']
 
 let hasil = []
 
+//array mana yang mau dipakai
 function array(x) {
     return (x)
 }
 
-function searchname(inis, max, callback) {
-    let tmp = callback.join('~').toLowerCase();
+function searchName(inis, max, callback) {
+    let tmp = callback.join('~').toLowerCase(); //menjadikan sting di array menjadi huruf kecil
     let Array = tmp.split('~');
     for (let i = 0; i < callback.length - 1; i++) {
         let y = Array[i];
-        let cari = y.includes(inis);
-        cari == true ? hasil.push(callback[i]) : "";
-        if (hasil.length >= max) break;
+        let cari = y.includes(inis); //proses mencari inis di setiap string
+        cari == true ? hasil.push(callback[i]) : ""; //proses memasukan sting yang benar ke array
+        if (hasil.length >= max) break; //proses pemberhentian pencarian kata maksimal yang dicari
     }
+    hasil.length == 0 ? hasil = "hasil tidak ditemukan" : ""; //proses jika inisial tidak ditemukan
     return (hasil)
 }
-console.log(searchname('an', 2, array(name)))
+console.log(searchName('ar', 2, array(name)))
